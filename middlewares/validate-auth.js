f69506b38efc78daa9c45c911e1c54a797940aee
Saúl Ramos Laborín <saul.ramos@necodex.com>
2025-08-07 09:51:41 -0700

    build: migrate to @clerk/express

--- middlewares/validate-auth.js
+++ middlewares/validate-auth.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 const Usuario = require("../models/usuario");
-const { ClerkExpressRequireAuth } = require("@clerk/clerk-sdk-node");
 const { response } = require("express");
+const { requireAuth } = require("@clerk/express");
 
-const validateAuth = ClerkExpressRequireAuth({
+const validateAuth = requireAuth({
   onError: (error) =>
     response.status(401).json({ message: "Acceso no autorizado", error }),
 });
